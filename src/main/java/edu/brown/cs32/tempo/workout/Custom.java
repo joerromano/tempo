f8fdb63c044b1a57b5432129e0a928cd69055a56
Simon Belete <simon_belete@brown.edu>
2016-04-13 14:52:56 -0400

    Custom Workout
    Just the class

---
+++ src/main/java/edu/brown/cs32/tempo/workout/Custom.java
@@ -0,0 +1,17 @@
+package edu.brown.cs32.tempo.workout;
+
+public class Custom extends Workout{
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String toHTML() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
