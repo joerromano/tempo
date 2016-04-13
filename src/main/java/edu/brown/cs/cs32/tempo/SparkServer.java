accc5986570d1332fa598d32008af23adc8ee2f3
thale1 <thomas_hale@brown.edu>
2016-04-12 22:19:02 -0400

    Further Spark setup

--- src/main/java/edu/brown/cs/cs32/tempo/SparkServer.java
+++ src/main/java/edu/brown/cs/cs32/tempo/SparkServer.java
@@ -4,20 +4,39 @@ import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.util.Collections;
+import java.util.Map;
 
 import org.json.JSONObject;
 
+import com.google.common.collect.ImmutableMap;
 import com.google.gson.Gson;
 
 import freemarker.template.Configuration;
 import spark.ExceptionHandler;
+import spark.ModelAndView;
 import spark.Request;
 import spark.Response;
 import spark.ResponseTransformer;
 import spark.Spark;
 import spark.template.freemarker.FreeMarkerEngine;
 
+/**
+ * SparkServer class. Runs a spark server at localhost:4567
+ *
+ * @author thale1
+ *
+ */
 public class SparkServer {
+  // TODO What freemarker files are we using?
+  private static final String HOME_FILE = "index.ftl";
+  private static final String SCHEDULE_FILE = null;
+  private static final String TEAM_FILE = null;
+  private static final String WORKOUT_FILE = null;
+  private static final String SETTINGS_FILE = null;
+  private static final String LOGIN_POPOVER = null;
+  private static final String TEAM_POPOVER = null;
+  private static final String NEW_WORKOUT_POPOVER = null;
   private final int PORT;
 
   /**
@@ -61,34 +80,48 @@ public class SparkServer {
     // return new ModelAndView(variables, "coachhome.ftl");
     // } , freeMarker);
     Spark.get("/home", (req, res) -> {
-      return null; // TODO
+      Map<String, Object> variables = ImmutableMap.of("title",
+          "Tempo: Your workout solution");
+      return new ModelAndView(variables, HOME_FILE); // TODO
     } , freeMarker);
     Spark.get("/schedule", (req, res) -> {
-      return null; // TODO
+      Map<String, Object> variables = ImmutableMap.of("title",
+          "Workout schedule");
+      return new ModelAndView(variables, SCHEDULE_FILE); // TODO
     } , freeMarker);
     Spark.get("/team/:id", (req, res) -> {
-      return null; // TODO
+      String id = req.params(":id");
+      // TODO get team with id, set title
+      String title = "Team " + id;
+      Map<String, Object> variables = ImmutableMap.of("title", title);
+      return new ModelAndView(variables, TEAM_FILE); // TODO
     } , freeMarker);
     Spark.get("/workout/:id", (req, res) -> {
-      return null; // TODO
+      String id = req.params(":id");
+      // TODO get workout with id, set title
+      String title = "Workout " + id;
+      Map<String, Object> variables = ImmutableMap.of("title", title);
+      return new ModelAndView(variables, WORKOUT_FILE); // TODO
     } , freeMarker);
     Spark.get("/settings", (req, res) -> {
-      return null; // TODO
+      // TODO how will authentication work?
+      Map<String, Object> variables = ImmutableMap.of("title", "Settings");
+      return new ModelAndView(variables, SETTINGS_FILE);
     } , freeMarker);
 
     Spark.post("/popover/login", (req, res) -> {
       // TODO return the login popover
-      return null;
+      return new ModelAndView(Collections.EMPTY_MAP, LOGIN_POPOVER);
     } , freeMarker);
 
     Spark.post("/popover/teamregister", (req, res) -> {
       // TODO return the team register popover
-      return null;
+      return new ModelAndView(Collections.EMPTY_MAP, TEAM_POPOVER);
     } , freeMarker);
 
     Spark.post("/popover/create_workout", (req, res) -> {
       // TODO return the new workout popover
-      return null;
+      return new ModelAndView(Collections.EMPTY_MAP, NEW_WORKOUT_POPOVER);
     } , freeMarker);
 
     // JSON-related POST requests
