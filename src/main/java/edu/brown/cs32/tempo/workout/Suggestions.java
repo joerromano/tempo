9314afb3058229435b4bd8c0373c89317bfc4d1c
Simon Belete <simon_belete@brown.edu>
2016-04-21 21:55:28 -0400

    Returns some workout suggestions
    Returns 5 suggested works when given a collection<athletes> or a single
    athlete

--- src/main/java/edu/brown/cs32/tempo/workout/Suggestions.java
+++ src/main/java/edu/brown/cs32/tempo/workout/Suggestions.java
@@ -2,11 +2,34 @@ package edu.brown.cs32.tempo.workout;
 
 import java.util.Collection;
 import java.util.Date;
+import java.util.ArrayList;
+import java.util.Calendar;
 
 import edu.brown.cs32.tempo.people.Athlete;
 
-interface Suggestions {	
-	public static Collection<Workout> getSuggestions(Collection<Athlete> a, Date date) {
-		return null;
+interface Suggestions {
+
+	public final static int NUM_WORKOUTS = 5;
+
+	public static Collection<Workout> getSuggestions(Collection<Athlete> athletes, Date date) {
+		ArrayList<Workout> retW = new ArrayList<Workout>();
+		for (Athlete a : athletes) {
+			retW.addAll(a.getWorkouts());
+		}
+
+		if (retW.size() > 4) {
+			return retW.subList(0, Suggestions.NUM_WORKOUTS);
+		} else {
+			return retW.subList(0, retW.size());
+		}
+	}
+
+	public static Collection<Workout> getSuggestions(Athlete athlete, Date date) {
+		ArrayList<Workout> retW = new ArrayList<Workout>(athlete.getWorkouts());
+		if (retW.size() > 4) {
+			return retW.subList(0, Suggestions.NUM_WORKOUTS);
+		} else {
+			return retW.subList(0, retW.size());
+		}
 	}
 }
