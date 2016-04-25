7bfd842fda0e8cc1a4e566797b6104a336902c85
Joe Romano <joerromano@users.noreply.github.com>
2016-04-24 22:14:04 -0400

    fixed ID in constructor so it shows up on the frontend properly

--- src/main/java/edu/brown/cs32/tempo/people/Person.java
+++ src/main/java/edu/brown/cs32/tempo/people/Person.java
@@ -3,57 +3,60 @@ package edu.brown.cs32.tempo.people;
 import edu.brown.cs32.tempo.location.PostalCode;
 
 public abstract class Person {
-	private String name; // Name of the person
-	private PostalCode location; // Location of the person
-	private String email;
-	private String id;
-	
-	public Person(String id, String email, String name, PostalCode location){
-		this.setEmail(email);
-		this.name = name;
-		this.location = location;
-	}
-	
-	/**
-	 * Gets the default location for the person
-	 * @return - the location for the person
-	 */
-	public PostalCode getLocation(){
-		return location;
-	}
-	
-	/**
-	 * Changes the default location for the person
-	 */
-	public void setLocation(PostalCode location){
-		this.location = location;
-	}
-	
-	/**
-	 * Get the name for the person
-	 * @return - the name of the person
-	 */
-	public String getName(){
-		return name;
-	}
-	
-	public void setName(String name){
-		this.name = name;
-	}
+  private String name; // Name of the person
+  private PostalCode location; // Location of the person
+  private String email;
+  private String id;
 
-	public String getEmail() {
-		return email;
-	}
+  public Person(String id, String email, String name, PostalCode location) {
+    this.id = id;
+    this.setEmail(email);
+    this.name = name;
+    this.location = location;
+  }
 
-	public void setEmail(String email) {
-		this.email = email;
-	}
+  /**
+   * Gets the default location for the person
+   * 
+   * @return - the location for the person
+   */
+  public PostalCode getLocation() {
+    return location;
+  }
 
-	public String getId() {
-		return id;
-	}
+  /**
+   * Changes the default location for the person
+   */
+  public void setLocation(PostalCode location) {
+    this.location = location;
+  }
 
-	public void setId(String id) {
-		this.id = id;
-	}
+  /**
+   * Get the name for the person
+   * 
+   * @return - the name of the person
+   */
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
 }
