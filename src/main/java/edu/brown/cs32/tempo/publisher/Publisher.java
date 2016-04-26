6c2fe5bcd0ea010f687ad5534de18453929d085f
thale1 <thomas_hale@brown.edu>
2016-04-26 10:41:33 -0400

    Set up the publisher to actually send emails.

--- src/main/java/edu/brown/cs32/tempo/publisher/Publisher.java
+++ src/main/java/edu/brown/cs32/tempo/publisher/Publisher.java
@@ -1,7 +1,10 @@
 package edu.brown.cs32.tempo.publisher;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.mail.internet.MimeMessage;
 
 import edu.brown.cs32.tempo.people.Athlete;
 import edu.brown.cs32.tempo.people.Group;
@@ -25,7 +28,20 @@ public class Publisher {
       }
       String email = a.getEmail();
       if (email != null) {
-
+        SimpleDateFormat format = new SimpleDateFormat("MMM d");
+        String subject = String.format("Workouts for week of %s",
+            format.format(g.getDate()));
+        String bodyText = String.format(
+            "Hi %s,\n\n"
+                + "Here are your tempo workouts for the week of %s:\n\n" + "%s",
+            a.getName(), format.format(g.getDate()), workoutIds);
+        try {
+          MimeMessage message = GmailSender.createEmail(email,
+              "tempo.brown.2016@gmail.com", subject, bodyText);
+          GmailSender.sendMessage(GmailSender.getGmailService(), "me", message);
+        } catch (Exception e) {
+          e.printStackTrace();
+        }
       }
     }
   }
