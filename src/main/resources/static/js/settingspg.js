e2591bb05ee6f67680c2595182585bc0c7dd936c
Joe Romano <joerromano@users.noreply.github.com>
2016-05-06 15:06:33 -0400

    removed update phone from frontend

--- src/main/resources/static/js/settingspg.js
+++ src/main/resources/static/js/settingspg.js
@@ -15,7 +15,7 @@ $(document).on('click', '#updateDemoBtn', function() {
     $.ajax({
         method: "POST",
         url: "/update",
-        data: JSON.stringify({name: $("#resetName").val(), phone: $("#resetPhone").val()}),
+        data: JSON.stringify({name: $("#resetName").val()}),
         success: function(responseJSON) { location.reload(); },
         error: function(XMLHttpRequest, textStatus, errorThrown) { 
             alert("Error: " + errorThrown + " | Your changes have not been applied");
